d8e8bdc66129f637728dc227c3c323c72f39cd8e
Jess Sepulveda <50001671+latinacoder@users.noreply.github.com>
2023-07-21 11:20:02 -0400

    new

--- codecademy/example1codecademy_solution.py
+++ codecademy/example1codecademy_solution.py
@@ -2,7 +2,7 @@
     #1. Iterate through list (i)
     #2. Iterate one over (j)
     #3. Iterate one over (k)
-    #4. Square and compare a**2 + b**2 = c**2
+    #4. Square and compate
 #2. explicit inputs and outputs
     #1. [3,4,5] = True
     #2. [4] = False
@@ -11,16 +11,16 @@
 
 
 def p_t(lst):
-    lst.sort() #this will assume list is in ascending order
-    for a in range(len(lst)): #iterate thru list "[a]"
-        for b in range(a+1,len(lst)): #iterate again but one over i to avoid duplicate variables "b"
-            for c in range(b+1,len(lst)): 
-                if lst[a]**2 + lst[b]**2 == lst[c]**2:  #if element at index a^2 plus element at index b^2 is equal to element at index b 
+    for i in range(len(lst)): #iterate thru list "a"
+        for j in range(i+1,len(lst)): #iterate again but one over i to avoid duplicate variables "b"
+            for k in range(j+1,len(lst)): #"c"
+                if lst[i]**2 + lst[j]**2 == lst[k]**2 #if element at index i^2 plus element at index j^2 is equal to element at index j 
                     return True
     return False
 
 
-lst = [4,3,5]
+lst = [3,4,5]
 print(p_t(lst))
+
 
 
